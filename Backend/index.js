81a1316927ea13fcab9e789adc5cc819cd63571a
Dev-KimDonghyun <hyungus723@gmail.com>
2025-06-07 14:41:51 +0900

    feat: Set Up ExpressJS to Connect to MongoDB Atlas

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,24 @@
-// Develop Start
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+const port = 5000;
+
+dotenv.config();
+
+const MONGOURI = process.env.MONGO_URI;
+
+app.use(express.json());
+
+const CheckMongoConnect = async () => {
+  try {
+    await mongoose.connect(MONGOURI);
+    console.log("Connected MongoDB Atlas");
+  } catch (err) {
+    console.log(`Failed to Connect MongoDB Atlas. Error Message is ${err}`);
+  }
+};
+
+CheckMongoConnect();
