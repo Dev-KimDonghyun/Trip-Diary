b23f54f5acd80d4925123407c486823fefc76a27
Dev-KimDonghyun <hyungus723@gmail.com>
2025-06-07 14:03:20 +0900

    feat: Set Up Base Routes Using React Router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 
 function App() {
   return (
-    <div>
-      <h1>Develop Start</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LogInPage />} />
+        <Route path="/signUp" element={<SignUpPage />} />
+        <Route path="/list" element={<ListPage/>} />
+        <Route path="/trip/new" element={<MakeTripPage />} />
+        <Route path="/trip/:id" element={<TripPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
