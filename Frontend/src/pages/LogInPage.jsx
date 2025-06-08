e812a5a85e2b9e4c28db317c7e373ceafd76c962
Dev-KimDonghyun <hyungus723@gmail.com>
2025-06-08 14:50:32 +0900

    feat: Add LogIn Logic without Backend Connection

--- Frontend/src/pages/LogInPage.jsx
+++ Frontend/src/pages/LogInPage.jsx
@@ -1,6 +1,24 @@
 const LogInPage = () => {
   const logInSubmitLogic = (e) => {
-    //
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const userId = formData.get("userId");
+    const userPW = formData.get("userPw");
+
+    if (!userId || !userPW) {
+      if (!userId) {
+        console.log("Enter a User ID");
+        alert("Enter a User ID");
+      } else if (!userPW) {
+        console.log("Enter a User PW");
+        alert("Enter a User PW");
+      } else {
+        console.log("Enter User ID and PW");
+        alert("Enter User ID and PW");
+      }
+    } else {
+      // LogIn JavaScript Logic
+    }
   };
   return (
     <div>
@@ -12,11 +30,23 @@ const LogInPage = () => {
           <input id="userPw" name="userPw" type="password" />
 
           <div>
-            <label>Find ID/PW</label>
+            <label
+              onClick={() => {
+                // Route To Find ID/PW Page
+              }}
+            >
+              Find ID/PW
+            </label>
           </div>
 
           <button type="submit">Log In</button>
-          <button>Sign Up</button>
+          <button
+            onClick={() => [
+              // Route To Sign Up Page
+            ]}
+          >
+            Sign Up
+          </button>
         </form>
       </div>
     </div>
