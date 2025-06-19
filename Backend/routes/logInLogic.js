248e92ee06ae6518bb8a633b8c06b36e0973be7b
Dev-KimDonghyun <hyungus723@gmail.com>
2025-06-20 08:59:27 +0900

    feat: Add Response Details After Successful User Login

--- Backend/routes/logInLogic.js
+++ Backend/routes/logInLogic.js
@@ -12,7 +12,14 @@ router.post("/login", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({ message: "Login successful", token });
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        _id: newUser._id,
+        userId: newUser.userId,
+        nickName: newUser.nickName,
+      },
+    });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Server Error" });
