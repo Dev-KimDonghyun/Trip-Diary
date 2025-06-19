d4cc0919234c402b944caf3e9adb6726d8b4024c
Dev-KimDonghyun <hyungus723@gmail.com>
2025-06-19 16:37:49 +0900

    refactor: Update Schema Models

--- Backend/models/createIdModel.js
+++ Backend/models/createIdModel.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     nickName: { type: String, require: true, uniqe: true },
     userId: { type: String, require: true, uniqe: true },
-    userPw: { type: String, require: true },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("createUser", userSchema);
--- Backend/models/joinIdModel.js
+++ Backend/models/joinIdModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     userId: { type: String, require: true, uniqe: true },
-    userPw: { type: String, require: true },
 });
 
-module.exports = mongoose.model("logInUser", userSchema);
+module.exports = mongoose.model("joinUser", userSchema);
