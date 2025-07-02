e685e4d7abbc07acc590f37dcd9bac35f3799b74
Dev-KimDonghyun <hyungus723@gmail.com>
2025-07-03 08:43:26 +0900

    refactor: Use useState for Input Handling on SignUp Page - Update Backend API Endpoint for SignUp

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
+
+  const [userId, setUserId] = useState("");
+  const [nickName, setNickName] = useState("");
 
   const SignUpPageFormList = [
     {
@@ -9,21 +13,21 @@ const SignUpPage = () => {
       type: "text",
       title: "Nickname",
       description: "Please enter your Nickname",
+      value: nickName,
+      change: (e) => setNickName(e.target.value),
     },
     {
       id: "createUserId",
       type: "text",
       title: "ID",
       description: "Please enter your ID",
+      value: userId,
+      change: (e) => setUserId(e.target.value),
     },
   ];
 
   const signUpSubmitLogic = async (e) => {
     e.preventDefault();
-
-    const signUpFormData = new FormData(e.currentTarget);
-    const nickName = signUpFormData.get("userNickName");
-    const userId = signUpFormData.get("createUserId");
 
     if (
       !userId ||
@@ -35,7 +39,7 @@ const SignUpPage = () => {
     } else {
       try {
         await axios
-          .post("http://localhost:5050/api/signup", {
+          .post("http://localhost:5050/api/createId", {
             nickName,
             userId,
           })
@@ -68,12 +72,12 @@ const SignUpPage = () => {
         <form onSubmit={signUpSubmitLogic}>
           {SignUpPageFormList.map((list) => (
             <div key={list.id}>
-              <label htmlFor={list.id}>{list.title}</label>
+              <label>{list.title}</label>
               <input
-                id={list.id}
-                name={list.id}
                 type={list.type}
+                value={list.value}
                 placeholder={list.description}
+                onChange={list.change}
               />
             </div>
           ))}
